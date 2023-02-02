5def1e09ff400c39730b1c74e3051829f78e7301
Joseph Adewunmi <adedejiosvaldo@gmail.com>
2023-02-02 14:27:35 +0100

    fix:added catch

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const scraper = require("./JumiaIndex");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Webscraper");
+});
+
 app.get("/api/jumia", async (req, res) => {
-  const scrapedJumiaCourses = await scraper.jumiaData;
+  const scrapedJumiaCourses = await scraper.jumiaData.catch((err) => {
+    console.error("err:", err);
+  });
 
   if (!scrapedJumiaCourses) res.status(400).send("Error From Your End");
   res.send(scrapedJumiaCourses);
 });
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  //   console.log("Port");
+});
