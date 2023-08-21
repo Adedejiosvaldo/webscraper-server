5056c6ff7646b5248b379829dbfcd53db4b9a788
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-21 12:43:04 +0100

    Fix:removed the express json

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.use("/api/v1/products", jumiaRoute);
-app.use(express.json)();
 
 app.get("/", (req, res) => {
   res.json({ msg: "success", location: "Homepage" });
